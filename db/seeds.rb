ae1e25ac6c9295bd9825f0843eb92f8aca8824f8
brandonaray <brandonray1225@gmail.com>
2019-03-03 11:06:50 -0600

    updates seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 Contact.create(first_name: "Brandon", last_name: "Ray", email: "brandonray1225@gmail.com", phone_number: "773-744-2520")
+Contact.create(first_name: "Kate", last_name: "Bauer", email: "kate.m.bauer@gmail.com", phone_number: "314-882-4567")
+Contact.create(first_name: "Charlie", last_name: "Ray", email: "charliebarley@gmail.com", phone_number: "312-123-9876")
