d15c5a14d6289a3f93002326ec50bb232aef935f
brandonaray <brandonray1225@gmail.com>
2019-03-03 11:04:01 -0600

    creates RESTful actions

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,11 +1,37 @@
 class Api::ContactsController < ApplicationController
-  def first_contact
-    @contact = Contact.first
-    render "first_contact.json.jbuilder"
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jbuilder"
   end
 
-  def all_contacts
+  def index
     @contacts = Contact.all
-    render "all_contacts.json.jbuilder"
+    render "index.json.jbuilder"
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    render "show.json.jbuilder"
+    @contact.save
+  end
+
+  def create
+    @contact = Contact.new
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.phone_number = params[:phone_number]
+    @contact.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    contact = Contact.find_by(id: params[:id])
+    contact.destroy
+    render json: {message: "Contact successfully deleted"}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Contact.create(first_name: "Brandon", last_name: "Ray", email: "brandonray1225@gmail.com", phone_number: "773-744-2520")
