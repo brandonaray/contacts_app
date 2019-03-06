85d6168b6efe2d04e37ce2d6d0779eefd07bb7c7
brandonaray <brandonray1225@gmail.com>
2019-03-05 20:19:15 -0600

    adds middle name and bio to contacts table

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -13,8 +13,10 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find_by(id: params[:id])
     @contact.first_name = params[:first_name] || @contact.first_name
     @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.bio = params[:bio] || @contact.bio
     if @contact.save
       render "show.json.jbuilder"
     else
@@ -25,9 +27,11 @@ class Api::ContactsController < ApplicationController
   def create
     @contact = Contact.new
     @contact.first_name = params[:first_name]
+    @contact.middle_name = params[:middle_name]
     @contact.last_name = params[:last_name]
     @contact.email = params[:email]
     @contact.phone_number = params[:phone_number]
+    @contact.bio = params[:bio] || @contact.bio
     if @contact.save
       render "show.json.jbuilder"
     else
--- app/views/api/contacts/_contact.json.jbuilder
+++ app/views/api/contacts/_contact.json.jbuilder
@@ -1,7 +1,9 @@
 json.id contact.id
 json.first_name contact.first_name
+json.middle_name contact.middle_name
 json.last_name contact.last_name
 json.full_name contact.full_name
+json.bio contact.bio
 json.email contact.email
 json.phone_number contact.japan_phone
 json.date_updated contact.friendly_updated_at
