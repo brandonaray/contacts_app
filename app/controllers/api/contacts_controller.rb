10e9272843e6578fa5aebf297166c3cc450e8e38
brandonaray <brandonray1225@gmail.com>
2019-03-11 20:53:03 -0500

    change contact create action to include user_id

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -31,13 +31,15 @@ class Api::ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new
-    @contact.first_name = params[:first_name]
-    @contact.middle_name = params[:middle_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.phone_number = params[:phone_number]
-    @contact.bio = params[:bio] || @contact.bio
+    @contact = Contact.new(
+      first_name = params[:first_name],
+      middle_name = params[:middle_name],
+      last_name = params[:last_name],
+      email = params[:email],
+      phone_number = params[:phone_number],
+      bio = params[:bio],
+      user_id = current_user.id
+      )
     if @contact.save
       render "show.json.jbuilder"
     else
